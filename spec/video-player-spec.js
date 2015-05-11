a99ed262e0278d1023d1818153b5164903a2c1ac
tyage <namatyage@gmail.com>
2015-05-12 00:43:06 +0900

    testコードを新しくした

--- spec/video-player-spec.js
+++ spec/video-player-spec.js
@@ -6,15 +6,32 @@ let remote = require('remote');
 let dialog = remote.require('dialog');
 
 describe("VideoPlayer", () => {
-  let activationPromise = null;
+  let activationPromise;
 
   beforeEach(() => {
-    atom.workspaceView = new WorkspaceView();
-    activationPromise = atom.packages.activatePackage('video-player');
+    expect(atom.packages.isPackageActive('video-player')).toBe(false);
+
+    atom.project.setPaths([path.join(__dirname, 'sample')]);
+
+    let workspaceElement = atom.views.getView(atom.workspace)
+
+    waitsForPromise(() => atom.workspace.open('README.md'));
+
+    runs(() => {
+      jasmine.attachToDOM(workspaceElement)
+      editor = atom.workspace.getActiveTextEditor()
+      editorView = atom.views.getView(editor)
+
+      activationPromise = atom.packages.activatePackage('video-player');
+      activationPromise.fail((e) => console.log(e));
+    });
   });
 
   describe("when the video-player:start event is triggered", () => {
     it("then dialog opened", () => {
+      atom.commands.dispatch(editorView, 'video-player:stop');
+      waitsForPromise(() => atom.packages.activatePackage('video-player'));
+
       let dialogOpened = false;
       let originalDialogOpen = dialog.showOpenDialog.bind(null);
       dialog.showOpenDialog = (...args) => {
@@ -22,11 +39,9 @@ describe("VideoPlayer", () => {
         return originalDialogOpen.call(dialog, ...args);
       };
 
-      atom.workspaceView.trigger('video-player:play');
+      atom.commands.dispatch(editorView, 'video-player:play');
 
-      waitsForPromise(() => activationPromise);
-
-      runs(() => expect(dialogOpened).toBe(true));
+      expect(dialogOpened).toBe(true);
     });
   });
 });
